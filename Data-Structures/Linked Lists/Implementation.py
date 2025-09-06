fc80844952a2af9b69f037d9574a5e079a99d4b0
Dina Metwalli <dina.moh657@gmail.com>
2025-09-06 15:38:45 +0100

    remove redundancy with LL node class

--- Data-Structures/Linked Lists/Implementation.py
+++ Data-Structures/Linked Lists/Implementation.py
@@ -1,55 +1,49 @@
 # Implementation of Linked List Data Structure
 
+class Node:
+    def __init__(self, value):
+        self.value = value
+        self.next = None
+
 class LinkedList:
     def __init__(self, value):
 
-        self.head = {
-            "value": value,
-            "next": None
-        }
+        self.head = Node(value)
         self.tail = self.head
         self.length = 1
 
     def append(self, value):
-        new_node = {
-            "value": value,
-            "next": None
-        }
-        self.tail["next"] = new_node
-        # remember that earlier tail was set to head because it was 1 node
-        # you dont want this to be head instead because it will only work when there are 2 nodes in the LL
-        # but if the LL size was more than 2 it will make head point to 3rd node+.
+        new_node = Node(value)
+        self.tail.next = new_node
         self.tail = new_node
         self.length += 1
 
     def prepend(self, value):
-        new_head = {
-            "value": value,
-            "next": self.head
-        }
+        new_head = Node(value)
+        new_head.next = self.head
         self.head = new_head
         self.length += 1
 
+    def print_list(self):
+        array = []
+        current_node = self.head
+        while current_node != None:
+            array.append(current_node.value)
+            current_node = current_node.next
+        print("Linked List: ", array, " Length: ", self.length)
 
 my_linked_list = LinkedList(10)
-print(my_linked_list.head)
-print(my_linked_list.length)
+my_linked_list.print_list()
 # 10 -> None. Size = 1
 
 my_linked_list.append(5)
-print(my_linked_list.tail)
-print(my_linked_list.head)
-print(my_linked_list.length)
+my_linked_list.print_list()
 # 10 -> 5 -> None. Size = 2
 
 my_linked_list.append(16)
-print(my_linked_list.tail)
-print(my_linked_list.head)
-print(my_linked_list.length)
+my_linked_list.print_list()
 # 10 -> 5 -> 16 -> None. Size = 3
 
 my_linked_list.prepend(1)
-print(my_linked_list.tail)
-print(my_linked_list.head)
-print(my_linked_list.length)
+my_linked_list.print_list()
 # 1 -> 10 -> 5 -> 16 -> None. Size = 4
